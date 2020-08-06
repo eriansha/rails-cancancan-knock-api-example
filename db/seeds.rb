8a00b091a9374a15477cfcc902c248f8b1536052
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-08-06 11:43:48 +0700

    Add product faker seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,15 @@
 User.create!(name: 'John', email: 'admin@tnklst.com', role: 'admin', password: '123456')
 User.create!(name: 'Adi', email: 'curation@tnklst.com', role: 'curation', password: '123456')
 User.create!(name: 'Jen', email: 'visitortnklst.com', role: 'visitor', password: '123456')
+
+10.times do |i|
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    brand: ['Zara', 'Uniqlo', 'H&M', 'Gucci'].sample,
+    category: Product.categories.values.sample,
+    colour: Faker::Color.color_name,
+    condition: Product.conditions.values.sample,
+    price: Faker::Number.decimal(l_digits: 6, r_digits: 2),
+    quantity: Faker::Number.number(digits: 2)
+  )
+end
