f60ece24845ab7ea4be6335074dd8edc8505b3c3
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-08-06 15:54:00 +0700

    Include CanCan::ControllerAdditions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::API
+  include CanCan::ControllerAdditions
   include Knock::Authenticable
   include API::V1
 
   rescue_from CanCan::AccessDenied do |exception|
     render json: { message: exception.message }, status: 403
   end
+  
 end
