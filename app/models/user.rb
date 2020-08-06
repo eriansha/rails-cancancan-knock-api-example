c6856cb58d3b3f57b768a3ca764728e8464bca5f
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-08-06 15:53:46 +0700

    Change token payloada

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
 
   def to_token_payload
     {
+      sub: self.id,
       name: self.name,
+      role: self.role,
       rules: Ability.new(self).to_list
     }
   end
