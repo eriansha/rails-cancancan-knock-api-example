6e2f17043860585533133afea8521e3327eed0c3
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-08-06 15:55:07 +0700

    Modify product controller

--- app/controllers/api/v1/admins/products_controller.rb
+++ app/controllers/api/v1/admins/products_controller.rb
@@ -1,18 +1,58 @@
-module Api
+module API
   module V1
     module Admins
-      #
       class ProductsController < ApplicationController
         before_action :authenticate_user
+        load_and_authorize_resource class: 'Product'
 
         def index
-          index_param
+          @products = Product.all
+          render(
+            json: @products,
+            root: 'products'
+          )
+        end
+
+        def create
+          Product.create!(
+            name: create_params[:name],
+            brand: create_params[:brand],
+            category: create_params[:category],
+            colour: create_params[:colour],
+            condition: create_params[:condition],
+            price: create_params[:price],
+            quantity: create_params[:quantity]
+          )
+        end
+
+        def show
+          product = Product.find_by(id: show_params[:id])
+          render(
+            json: product,
+            root: 'product'
+          )
         end
 
         private
 
         def index_param
           params.merge(current_user)
+        end
+
+        def show_params
+          params.permit(:id)
+        end
+
+        def create_params
+          params.permit(
+            :name,
+            :brand,
+            :category,
+            :color,
+            :condition,
+            :price,
+            :quantity
+          )
         end
 
       end
